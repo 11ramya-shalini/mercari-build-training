491e1d0634443ec29db5361678bbdc4e1af1ce3f
Ramya Shalini <ramyashalini2005@gmail.com>
2025-03-27 19:07:26 +0530

    Completed STEP 10: Docker compose

--- python/main.py
+++ python/main.py
@@ -145,7 +145,7 @@ origins = [os.environ.get("FRONT_URL", "http://localhost:3000")]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=False,
+    allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
